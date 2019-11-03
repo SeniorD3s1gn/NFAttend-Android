9bf9bcdebf1ad212da5b8c611769eeaf7fc9abb7
Wilson <wilsonjang10@gmail.com>
2019-11-03 14:08:34 -0500

    Created course page and course item template

---
+++ app/src/main/java/com/nfa/android/CourseActivity.java
@@ -0,0 +1,30 @@
+package com.nfa.android;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CourseActivity extends AppCompatActivity {
+
+    private TextView CourseInfo;
+    private TextView CourseType;
+    private TextView CourseTime;
+    private TextView CourseLocation;
+    private TextView CoursePro;
+    private CalendarView CourseCal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.course_page);
+
+        CourseInfo = findViewById(R.id.CourseInfo);
+        CourseType = findViewById(R.id.CourseType);
+        CourseTime = findViewById(R.id.CourseTime);
+        CourseLocation = findViewById(R.id.CourseLocation);
+        CoursePro = findViewById(R.id.CoursePro);
+        CourseCal = findViewById(R.id.CourseCal);
+
+    }
+}
